94a1746b97020731c39a064262a27ab92e5e0ab7
saigirishgilly98 <saigirishgilly98@gmail.com>
2020-02-29 01:51:28 +0530

    Created newDistanceMatrix based on selected rackno

--- app/src/main/java/com/example/loweproject/MainActivity.java
+++ app/src/main/java/com/example/loweproject/MainActivity.java
@@ -21,6 +21,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
@@ -107,6 +108,22 @@ public class MainActivity extends ListActivity {
             @Override
             public void onClick(View v) {
                 try {
+
+                    int[][] distanceMatrix = {
+                            {0, 2451, 713, 1018, 1631, 1374, 2408, 213, 2571, 875, 1420, 2145, 1972},
+                            {2451, 0, 1745, 1524, 831, 1240, 959, 2596, 403, 1589, 1374, 357, 579},
+                            {713, 1745, 0, 355, 920, 803, 1737, 851, 1858, 262, 940, 1453, 1260},
+                            {1018, 1524, 355, 0, 700, 862, 1395, 1123, 1584, 466, 1056, 1280, 987},
+                            {1631, 831, 920, 700, 0, 663, 1021, 1769, 949, 796, 879, 586, 371},
+                            {1374, 1240, 803, 862, 663, 0, 1681, 1551, 1765, 547, 225, 887, 999},
+                            {2408, 959, 1737, 1395, 1021, 1681, 0, 2493, 678, 1724, 1891, 1114, 701},
+                            {213, 2596, 851, 1123, 1769, 1551, 2493, 0, 2699, 1038, 1605, 2300, 2099},
+                            {2571, 403, 1858, 1584, 949, 1765, 678, 2699, 0, 1744, 1645, 653, 600},
+                            {875, 1589, 262, 466, 796, 547, 1724, 1038, 1744, 0, 679, 1272, 1162},
+                            {1420, 1374, 940, 1056, 879, 225, 1891, 1605, 1645, 679, 0, 1017, 1200},
+                            {2145, 357, 1453, 1280, 586, 887, 1114, 2300, 653, 1272, 1017, 0, 504},
+                            {1972, 579, 1260, 987, 371, 999, 701, 2099, 600, 1162, 1200, 504, 0} };
+
                     InputStream is = getAssets().open("array.txt");
                     InputStream is2 = getAssets().open("arrayrack.txt");
 
@@ -152,18 +169,58 @@ public class MainActivity extends ListActivity {
                             }
                         }
                     }
+                    int[] rackorder = new int[k];
+                    int t = 0;
                     for(i=0;i<rackList.length;i++) {
                         if (rackList[i] != -1) {
-                            rackorder = rackorder + rackList[i] + "->";
+                            rackorder[t] = rackList[i];
+                            t++;
                         }
                         Log.d(MainActivity.TAG,""+rackList[i]);
                     }
-                    rackorder = rackorder + "0";
-                    Toast.makeText(MainActivity.this, rackorder, Toast.LENGTH_SHORT).show();
-
-
-                    //Google-TSP
-
+                    int[] uniquerackorder = new int[t];
+                    for(i=0;i<t;i++)
+                        uniquerackorder[i] = -1;
+                    int l=0,flag=0;
+                    for(i = 0; i < t; i++)
+                    {
+                        for(j=0;j<t;j++)
+                        {
+                           if(rackorder[i] == uniquerackorder[j]){
+                               flag = 1;
+                           }
+                        }
+                        if(flag == 0)
+                        {
+                            uniquerackorder[l] = rackorder[i];
+                            l++;
+                        }
+                        else{
+                            flag = 0;
+                        }
+                    }
+                    int temp;
+                    for (i = 0; i < l; i++)
+                    {
+                        for (j = i + 1; j < l; j++) {
+                            if (uniquerackorder[i] > uniquerackorder[j])
+                            {
+                                temp = uniquerackorder[i];
+                                uniquerackorder[i] = uniquerackorder[j];
+                                uniquerackorder[j] = temp;
+                            }
+                        }
+                    }
+                    int[][] newDistanceMatrix = new int[uniquerackorder.length][uniquerackorder.length];
+                    for(i=0;i<l;i++)
+                    {
+                        for(j=0;j<l;j++)
+                        {
+                            newDistanceMatrix[i][j] = distanceMatrix[uniquerackorder[i]][uniquerackorder[j]];
+                            Log.d(MainActivity.TAG,""+newDistanceMatrix[i][j]);
+                        }
+                    }
+                    //Toast.makeText(MainActivity.this, rackorder, Toast.LENGTH_SHORT).show();
 
 
                     for(i=0;i<rackList.length;i++)
