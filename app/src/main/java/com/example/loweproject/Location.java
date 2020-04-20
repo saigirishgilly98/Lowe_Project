979b4524e884e954b3476510a1caa8b1ab2543b9
saigirishgilly98 <saigirishgilly98@gmail.com>
2020-04-20 15:42:31 +0530

    Added the navigation path and images

--- app/src/main/java/com/example/loweproject/Location.java
+++ app/src/main/java/com/example/loweproject/Location.java
@@ -2,13 +2,307 @@ package com.example.loweproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Location extends AppCompatActivity {
+
+    private static final String TAG = Location.class.getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location);
+
+        final ImageView rack = findViewById(R.id.rack);
+        final ImageView map = findViewById(R.id.map);
+        Button btnPrev = findViewById(R.id.btnPrev);
+        Button btnNext = findViewById(R.id.btnNext);
+        Button btnClose = findViewById(R.id.btnClose);
+
+        final int[][][] imageArray = {{{0}, {0, 3}, {0, 3}, {1, 17, 7}, {1, 17, 7}, {1, 19, 21, 13}, {1, 19, 21, 13}, {0, 3, 26, 5}, {0, 3, 26, 5}, {0, 3, 32, 29, 11}, {0, 3, 32, 29, 10}, {0, 3, 32, 30, 34, 15}, {0, 3, 32, 30, 34, 15}},
+                {{16}, {3}, {4}, {16, 1, 17, 7}, {16, 1, 17, 7}, {16, 1, 19, 21, 13}, {16, 1, 19, 21, 13}, {3, 26, 5}, {3, 26, 5}, {3, 32, 29, 11}, {3, 32, 29, 10}, {3, 32, 30, 34, 15}, {3, 32, 30, 34, 15}},
+                {{16}, {3}, {4}, {16, 1, 17, 7}, {16, 1, 17, 7}, {16, 1, 19, 21, 13}, {16, 1, 19, 21, 13}, {3, 26, 5}, {3, 26, 5}, {3, 32, 29, 11}, {3, 32, 29, 10}, {3, 32, 30, 34, 15}, {3, 32, 30, 34, 15}},
+                {{18, 20, 0}, {18, 20, 0, 3}, {18, 20, 0, 3}, {7}, {7}, {18, 19, 21, 13}, {18, 19, 21, 13}, {7, 31, 26, 5}, {7, 31, 26, 5}, {7, 29, 11}, {7, 29, 10}, {7, 30, 34, 15}, {7, 30, 34, 15}},
+                {{18, 20, 0}, {18, 20, 0, 3}, {18, 20, 0, 3}, {7}, {7}, {18, 19, 21, 13}, {18, 19, 21, 13}, {7, 31, 26, 5}, {7, 31, 26, 5}, {7, 29, 11}, {7, 29, 10}, {7, 30, 34, 15}, {7, 30, 34, 15}},
+                {{24, 22, 20, 0}, {24, 22, 20, 0, 3}, {24, 22, 20, 0, 3}, {24, 22, 17, 7}, {24, 22, 17, 7}, {13}, {13}, {13, 36, 31, 26, 5}, {13, 36, 31, 26, 5}, {13, 36, 29, 11}, {13, 36, 29, 10}, {13, 34, 15}, {13, 34, 15}},
+                {{24, 22, 20, 0}, {24, 22, 20, 0, 3}, {24, 22, 20, 0, 3}, {24, 22, 17, 7}, {24, 22, 17, 7}, {13}, {13}, {13, 36, 31, 26, 5}, {13, 36, 31, 26, 5}, {13, 36, 29, 11}, {13, 36, 29, 10}, {13, 34, 15}, {13, 34, 15}},
+                {{27, 25, 16, 0}, {27, 25, 16}, {27, 25, 16}, {27, 32, 28, 18}, {27, 32, 28, 18}, {27, 32, 30, 33, 24}, {27, 32, 30, 33, 24}, {5}, {5}, {27, 32, 29, 11}, {27, 32, 29, 10}, {27, 32, 30, 34, 15}, {27, 32, 30, 34, 15}},
+                {{27, 25, 16, 0}, {27, 25, 16}, {27, 25, 16}, {27, 32, 28, 18}, {27, 32, 28, 18}, {27, 32, 30, 33, 24}, {27, 32, 30, 33, 24}, {5}, {5}, {27, 32, 29, 11}, {27, 32, 29, 10}, {27, 32, 30, 34, 15}, {27, 32, 30, 34, 15}},
+                {{23, 28, 18, 20, 0}, {23, 31, 25, 16}, {23, 31, 25, 16}, {23, 28, 18}, {23, 28, 18}, {23, 30, 33, 24}, {23, 30, 33, 24}, {23, 31, 26, 5}, {23, 31, 26, 5}, {11}, {10}, {23, 30, 34, 15}, {23, 30, 34, 15}},
+                {{23, 28, 18, 20, 0}, {23, 31, 25, 16}, {23, 31, 25, 16}, {23, 28, 18}, {23, 28, 18}, {23, 30, 33, 24}, {23, 30, 33, 24}, {23, 31, 26, 5}, {23, 31, 26, 5}, {11}, {10}, {23, 30, 34, 15}, {23, 30, 34, 15}},
+                {{35, 33, 24, 22, 20, 0}, {35, 36, 31, 25, 16}, {35, 36, 31, 25, 16}, {35, 36, 28, 18}, {35, 36, 28, 18}, {35, 33, 24}, {35, 33, 24}, {35, 36, 31, 26, 5}, {35, 36, 31, 26, 5}, {35, 36, 29, 11}, {35, 36, 29, 10}, {15}, {15}},
+                {{35, 33, 24, 22, 20, 0}, {35, 36, 31, 25, 16}, {35, 36, 31, 25, 16}, {35, 36, 28, 18}, {35, 36, 28, 18}, {35, 33, 24}, {35, 33, 24}, {35, 36, 31, 26, 5}, {35, 36, 31, 26, 5}, {35, 36, 29, 11}, {35, 36, 29, 10}, {15}, {15}}
+        };
+
+        Intent intent = getIntent();
+        String path = intent.getStringExtra("path");
+        final ArrayList<String> product = intent.getStringArrayListExtra("product");
+        int i, size = 0;
+        final int[] array_product_rack = new int[product.size()];
+        for(i = 0; i < path.length(); i++ ) {
+            char c = path.charAt(i);
+            if(Character.isDigit(c)) {
+                size++;
+            }
+        }
+
+
+
+        final int[] arr_path = new int[size];
+        final ArrayList<Integer> arr_product= new ArrayList<Integer>();
+        int k = 0;
+        Pattern p = Pattern.compile("\\d+");
+        Matcher m = p.matcher(path);
+        while(m.find()) {
+            arr_path[k++] = Integer.parseInt(m.group());
+        }
+
+        try {
+            InputStream is = getAssets().open("optimalrack.txt");
+
+            int size1 = is.available();
+
+            byte[] buffer_rack = new byte[size1];
+            is.read(buffer_rack);
+            is.close();
+
+
+
+            // Convert the buffer into a string.
+            String text_rack = new String(buffer_rack);
+
+            String lines_rack[] = text_rack.split("\\r?\\n");
+
+
+
+
+
+
+        for(i=0; i < arr_path.length; i++)
+            Log.d(Location.TAG, "Array Path: " + arr_path[i]);
+
+        View.OnClickListener listenerClose = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Location.this, MainActivity.class);
+                startActivity(intent);
+            }
+        };
+
+        final int imagePath[] = {R.drawable.path_0, R.drawable.path_1, R.drawable.path_2, R.drawable.path_3, R.drawable.path_4, R.drawable.path_5, R.drawable.path_6, R.drawable.path_7,
+                R.drawable.path_8, R.drawable.path_9, R.drawable.path_10, R.drawable.path_11, R.drawable.path_12, R.drawable.path_13,R.drawable.path_14, R.drawable.path_15, R.drawable.path_16,
+                R.drawable.path_17, R.drawable.path_18, R.drawable.path_19, R.drawable.path_20, R.drawable.path_21, R.drawable.path_22, R.drawable.path_23, R.drawable.path_24, R.drawable.path_25,
+                R.drawable.path_26, R.drawable.path_27, R.drawable.path_28, R.drawable.path_29, R.drawable.path_30, R.drawable.path_31, R.drawable.path_32, R.drawable.path_33, R.drawable.path_34,
+                R.drawable.path_35, R.drawable.path_36};
+
+        final int imageMap[] = {R.drawable.map_0, R.drawable.map_1, R.drawable.map_2, R.drawable.map_3, R.drawable.map_4, R.drawable.map_5, R.drawable.map_6, R.drawable.map_7, R.drawable.map_8,
+                R.drawable.map_9, R.drawable.map_10, R.drawable.map_11};
+
+        int j;
+        final ArrayList<Integer> listPath = new ArrayList<Integer>();
+        for(i = 1; i < arr_path.length; i++) {
+            int[] images = imageArray[arr_path[i-1]][arr_path[i]];
+            for(j=0;j<images.length; j++)
+                listPath.add(images[j]);
+        }
+
+        final int[] count = {0};
+        final String[] product_output = {""};
+
+        int[][] product_display_rack = {{0}, {3, 4, 16}, {3, 4, 6}, {7, 8, 9, 18}, {7, 8, 9, 18}, {13, 14, 24}, {13, 14, 24}, {5, 2, 7}, {5, 2, 7}, {10, 11, 23}, {10, 11, 23}, {15, 35}, {15, 35}};
+
+        View.OnClickListener listenerNext = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rack.setImageResource(imagePath[listPath.get(count[0])]);
+                switch (listPath.get(count[0])) {
+                    case 0 :
+                    case 1 : map.setImageResource(imageMap[0]);
+                             break;
+                    case 3 :
+                    case 4 :
+                    case 16 : map.setImageResource(imageMap[1]);
+                            break;
+                    case 17 :
+                    case 19 :
+                    case 20 : map.setImageResource(imageMap[2]);
+                            break;
+                    case 7 :
+                    case 8 :
+                    case 9 :
+                    case 18 : map.setImageResource(imageMap[3]);
+                            break;
+                    case 21 :
+                    case 22 : map.setImageResource(imageMap[4]);
+                            break;
+                    case 13 :
+                    case 14 :
+                    case 24 : map.setImageResource(imageMap[5]);
+                            break;
+                    case 25 :
+                    case 26 :
+                    case 32 : map.setImageResource(imageMap[6]);
+                            break;
+                    case 5 :
+                    case 27 : map.setImageResource(imageMap[7]);
+                            break;
+                    case 28 :
+                    case 29 :
+                    case 30 :
+                    case 31 : map.setImageResource(imageMap[8]);
+                            break;
+                    case 10 :
+                    case 11 :
+                    case 23 : map.setImageResource(imageMap[9]);
+                            break;
+                    case 33 :
+                    case 34 :
+                    case 36 : map.setImageResource(imageMap[10]);
+                            break;
+                    case 15 :
+                    case 35 : map.setImageResource(imageMap[11]);
+                            break;
+                    default: map.setImageResource(imageMap[0]);
+
+                }
+
+
+                if(count[0] < listPath.size() - 1) {
+                    count[0]++;
+                } else {
+                    Toast.makeText(Location.this, "Journey Completed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
+
+        View.OnClickListener listenerPrev = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(count[0] > 0) {
+                    count[0]--;
+                    rack.setImageResource(imagePath[listPath.get(count[0])]);
+                    switch (listPath.get(count[0])) {
+                        case 0 :
+                        case 1 : map.setImageResource(imageMap[0]);
+                            break;
+                        case 3 :
+                        case 4 :
+                        case 16 : map.setImageResource(imageMap[1]);
+                            break;
+                        case 17 :
+                        case 19 :
+                        case 20 : map.setImageResource(imageMap[2]);
+                            break;
+                        case 7 :
+                        case 8 :
+                        case 9 :
+                        case 18 : map.setImageResource(imageMap[3]);
+                            break;
+                        case 21 :
+                        case 22 : map.setImageResource(imageMap[4]);
+                            break;
+                        case 13 :
+                        case 14 :
+                        case 24 : map.setImageResource(imageMap[5]);
+                            break;
+                        case 25 :
+                        case 26 :
+                        case 32 : map.setImageResource(imageMap[6]);
+                            break;
+                        case 5 :
+                        case 27 : map.setImageResource(imageMap[7]);
+                            break;
+                        case 28 :
+                        case 29 :
+                        case 30 :
+                        case 31 : map.setImageResource(imageMap[8]);
+                            break;
+                        case 10 :
+                        case 11 :
+                        case 23 : map.setImageResource(imageMap[9]);
+                            break;
+                        case 33 :
+                        case 34 :
+                        case 36 : map.setImageResource(imageMap[10]);
+                            break;
+                        case 15 :
+                        case 35 : map.setImageResource(imageMap[11]);
+                            break;
+                        default: map.setImageResource(imageMap[0]);
+
+                    }
+                } else {
+                    Toast.makeText(Location.this, "Start Reached", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        };
+
+
+        /** Setting the event listener for the Prev button */
+        btnPrev.setOnClickListener(listenerPrev);
+
+        /** Setting the event listener for the Next button */
+        btnNext.setOnClickListener(listenerNext);
+
+        /** Setting the event listener for the Close button */
+        btnClose.setOnClickListener(listenerClose);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    //To display custom dialog
+    private void showCustomDialog(String var_rack_order) {
+        //before inflating the custom alert dialog layout, we will get the current activity viewgroup
+        ViewGroup viewGroup = findViewById(android.R.id.content);
+
+
+        //then we will inflate the custom alert dialog xml that we created
+        View dialogView = LayoutInflater.from(this).inflate(R.layout.my_dialog, viewGroup, false);
+
+        Button buttonOk = (Button) dialogView.findViewById(R.id.buttonOk);
+
+        TextView txtPath = (TextView) dialogView.findViewById(R.id.txtPath);
+        txtPath.setText(var_rack_order);
+
+        //Now we need an AlertDialog.Builder object
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        //setting the view of the builder to our custom view that we already inflated
+        builder.setView(dialogView);
+
+        //finally creating the alert dialog and displaying it
+        final AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+
+        View.OnClickListener listenerOk = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alertDialog.dismiss();
+            }
+        };
+
+
+        buttonOk.setOnClickListener(listenerOk);
+
     }
 }
--- app/src/main/java/com/example/loweproject/MainActivity.java
+++ app/src/main/java/com/example/loweproject/MainActivity.java
@@ -385,8 +385,236 @@ public class MainActivity extends ListActivity {
         View.OnClickListener listenerNav = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this, Location.class);
-                startActivity(i);
+                try {
+
+                    //Checking if the list is empty
+                    if (listArray.size() == 0)
+                        Toast.makeText(MainActivity.this, "List is empty!", Toast.LENGTH_SHORT).show();
+
+                    /**
+                     * Initializing the distanceMatrix
+                     * in which distanceMatrix[i,j] refers to the distance between rack i to rack j
+                     */
+                    double[][] distanceMatrix = {
+                            {0, 4, 4,7, 7, 10, 10, 10.5, 10.5, 13.5, 13.5, 16.5, 16.5},
+                            {4, 0, 0, 11, 11, 14, 14, 6.5, 6.5, 9.5, 9.5, 12.5, 12.5},
+                            {4, 0, 0, 11, 11, 14, 14, 6.5, 6.5, 9.5, 9.5, 12.5, 12.5},
+                            {7, 11, 11, 0, 0, 11, 11, 9.5, 9.5, 6.5, 6.5, 9.5, 9.5},
+                            {7, 11, 11, 0, 0, 11, 11, 9.5, 9.5, 6.5, 6.5, 9.5, 9.5},
+                            {10, 14, 14, 11, 11, 0, 0, 12.5, 12.5, 9.5, 9.5, 6.5, 6.5},
+                            {10, 14, 14, 11, 11, 0, 0, 12.5, 12.5, 9.5, 9.5, 6.5, 6.5},
+                            {10.5, 6.5, 6.5, 9.5, 9.5, 12.5, 12.5, 0, 0, 8, 8, 11, 11},
+                            {10.5, 6.5, 6.5, 9.5, 9.5, 12.5, 12.5, 0, 0, 8, 8, 11, 11},
+                            {13.5, 9.5, 9.5, 6.5, 6.5, 9.5, 9.5, 8, 8, 0, 0, 8, 8},
+                            {13.5, 9.5, 9.5, 6.5, 6.5, 9.5, 9.5, 8, 8, 0, 0, 8, 8},
+                            {16.5, 12.5, 12.5, 9.5, 9.5, 6.5, 6.5, 11, 11, 8, 8, 0, 0},
+                            {16.5, 12.5, 12.5, 9.5, 9.5, 6.5, 6.5, 11, 11, 8, 8, 0, 0}};
+
+                    /**
+                     * opening the files
+                     */
+                    InputStream is = getAssets().open("optimaluniqueproduct.txt");
+                    InputStream is2 = getAssets().open("optimalrack.txt");
+                    InputStream is3 = getAssets().open("optimalpath.txt");
+
+                    // We guarantee that the available method returns the total
+                    // size of the asset...  of course, this does mean that a single
+                    // asset can't be more than 2 gigs.
+                    int size1 = is.available();
+                    int size2 = is2.available();
+                    int size3 = is3.available();
+
+                    // Read the entire asset into a local byte buffer.
+                    byte[] buffer_product = new byte[size1];
+                    is.read(buffer_product);
+                    is.close();
+
+                    byte[] buffer_rack = new byte[size2];
+                    is2.read(buffer_rack);
+                    is2.close();
+
+                    byte[] buffer_path = new byte[size3];
+                    is3.read(buffer_path);
+                    is3.close();
+
+                    // Convert the buffer into a string.
+                    String text_product = new String(buffer_product);
+
+                    String lines_product[] = text_product.split("\\r?\\n");
+
+                    String text_rack = new String(buffer_rack);
+
+                    String lines_rack[] = text_rack.split("\\r?\\n");
+
+                    String text_path = new String(buffer_path);
+
+                    String lines_path[] = text_path.split("\\r?\\n");
+
+
+                    //Declaring rackList which is used to store the rack numbers of entered list items
+                    int[] rackList = new int[listArray.size()];
+
+                    //Initializing to rackList array to -1
+                    for (i = 0; i < rackList.length; i++) {
+                        rackList[i] = -1;
+                    }
+
+
+                    int k = 0;
+
+                    ArrayList<String> product_pass = new ArrayList<String>();
+
+                    //Storing all the rack numbers of entered list items
+                    for (i = 0; i < listArray.size(); i++) {
+                        product_name = listArray.get(i);
+                        Log.d(MainActivity.TAG, product_name);
+                        for (j = 0; j < 100; j++) {
+                            if (product_name.equals(lines_product[j])) {
+                                Log.d(MainActivity.TAG, "Rack No: " + lines_rack[j]);
+                                product_pass.add(product_name);
+                                rackList[k] = Integer.parseInt(lines_rack[j]);
+                                k++;
+                                break;
+                            }
+                        }
+                    }
+                    //Declaring Rackorder to get definite number of rack numbers without impure(-1) values
+                    Integer[] rackorder = new Integer[k];
+                    int t = 0;
+                    int z = 0;
+                    for (i = 0; i < rackList.length; i++) {
+                        if (rackList[i] != -1) {
+                            rackorder[t] = rackList[i];
+                            t++;
+                        }
+                        Log.d(MainActivity.TAG, "" + rackList[i]);
+                    }
+                    for (i = 0; i < rackorder.length; i++)
+                        Log.d(MainActivity.TAG, "rackorder:" + rackorder[i]);
+
+                    //Create set from array elements
+                    LinkedHashSet<Integer> linkedHashSet = new LinkedHashSet<>(Arrays.asList(rackorder));
+
+                    //Get back the array without duplicates
+                    Integer[] uniquerackorder = linkedHashSet.toArray(new Integer[]{});
+
+
+
+                    int temp;
+                    //Sorting the unique rack order
+                    for (i = 0; i < uniquerackorder.length; i++) {
+                        for (j = i + 1; j < uniquerackorder.length; j++) {
+                            if (uniquerackorder[i] > uniquerackorder[j]) {
+                                temp = uniquerackorder[i];
+                                uniquerackorder[i] = uniquerackorder[j];
+                                uniquerackorder[j] = temp;
+                            }
+                        }
+                    }
+
+                    //Adding 0 to the beginning and end of the unique rack order
+                    Integer[] temp_uniquerackorder = new Integer[uniquerackorder.length + 2] ;
+                    temp_uniquerackorder[0] = 0;
+                    for(i = 0; i < uniquerackorder.length; i++) {
+                        temp_uniquerackorder[i+1] = uniquerackorder[i];
+                    }
+                    temp_uniquerackorder[i+1] = 0;
+
+                    for(i=0; i<temp_uniquerackorder.length; i++)
+                        Log.d(MainActivity.TAG, "temp uniquerackorder: " + temp_uniquerackorder[i]);
+
+                    /**
+                     * Declaring and initializing newDistanceMatrix to only contain the distances between
+                     * the rack numbers of entered list items.
+                     */
+                    /*double[][] newDistanceMatrix = new double[temp_uniquerackorder.length][temp_uniquerackorder.length];
+                    for (i = 0; i < temp_uniquerackorder.length; i++) {
+                        for (j = 0; j < temp_uniquerackorder.length; j++) {
+                            newDistanceMatrix[i][j] = distanceMatrix[temp_uniquerackorder[i]][temp_uniquerackorder[j]];
+                        }
+                    }
+
+                     */
+
+                    /**
+                     * Getting the shortest path by comparing the obtained rack numbers with the
+                     * same rack numbers in any order present in paths.txt file
+                     */
+                    int rackflag = 0;
+                    for (i = 0; i < (lines_path.length); i++) {
+                        String[] tokens = lines_path[i].split(" ");
+                        Integer[] array_path = new Integer[tokens.length];
+                        int y = 0;
+                        for (String token : tokens) {
+                            array_path[y++] = Integer.parseInt(token);
+                        }
+
+                        if (compareArrays(array_path, temp_uniquerackorder)) {
+
+                            for (i = 0; i < ((array_path.length) - 1); i++) {
+                                var_rack_order += "" + array_path[i] + "->";
+                            }
+                            var_rack_order += array_path[i];
+                            rackflag = 1;
+                            break;
+                        }
+                    }
+                    /**
+                     * If shortest rack order found then sort rack order and corresponding product name in
+                     * ascending order according to rack no
+                     */
+                    if (rackflag == 1) {
+                        rackflag = 0;
+                        String output = "";
+                        String finalOutput = "";
+                        Integer[] sortedRackOrder = new Integer[rackorder.length];
+                        String[] sortedListArray = new String[listArray.size()];
+                        for (i = 0; i < rackorder.length; i++) {
+                            sortedRackOrder[i] = rackorder[i];
+                            sortedListArray[i] = listArray.get(i);
+                        }
+                        int temp1;
+                        String temp2;
+                        for (i = 0; i < sortedRackOrder.length; i++) {
+                            for (j = i + 1; j < sortedRackOrder.length; j++) {
+                                if (sortedRackOrder[i] > sortedRackOrder[j]) {
+                                    temp1 = sortedRackOrder[i];
+                                    sortedRackOrder[i] = sortedRackOrder[j];
+                                    sortedRackOrder[j] = temp1;
+                                    temp2 = sortedListArray[i];
+                                    sortedListArray[i] = sortedListArray[j];
+                                    sortedListArray[j] = temp2;
+                                }
+                            }
+                        }
+
+
+                        //Obtaining the final order in required format and displaying it using customDialog
+                        finalOutput += "The Shortest path is : \n" + var_rack_order + "\n----------------------------\n";
+                        for (i = 0; i < sortedRackOrder.length; i++)
+                            output += "" + sortedRackOrder[i] + "->" + sortedListArray[i] + "\n" + "----------------------------" + "\n";
+
+                        finalOutput += output;
+                        Log.d(MainActivity.TAG, "finalOutput: " + finalOutput);
+                        //showCustomDialog(finalOutput);
+                        Intent i = new Intent(MainActivity.this, Location.class);
+                        i.putExtra("path", var_rack_order);
+                        i.putExtra("product", product_pass);
+                        startActivity(i);
+                    }
+
+
+                    var_rack_order = "";
+
+
+                    for (i = 0; i < rackList.length; i++)
+                        rackList[i] = -1;
+
+
+                } catch (IOException e) {
+                    // Should never happen!
+                    throw new RuntimeException(e);
+                }
             }
         };
 
@@ -401,6 +629,7 @@ public class MainActivity extends ListActivity {
         /** Setting the event listener for the done button */
         btnDone.setOnClickListener(listenerDone);
 
+        /** Setting the event listener for the Navigate button */
         btnNav.setOnClickListener(listenerNav);
 
         /** Setting the adapter to the ListView */
@@ -457,7 +686,7 @@ public class MainActivity extends ListActivity {
     private void fillCountryList() {
         countryList = new ArrayList<>();
         try {
-            InputStream is = getAssets().open("array.txt");
+            InputStream is = getAssets().open("optimaluniqueproduct.txt");
 
             // We guarantee that the available method returns the total
             // size of the asset...  of course, this does mean that a single
@@ -475,7 +704,7 @@ public class MainActivity extends ListActivity {
             //Spliting each line by using new line regex expression
             String lines[] = text.split("\\r?\\n");
             //Adding each item to the list
-            for (i = 0; i < 3620; i++)
+            for (i = 0; i < 100; i++)
                 countryList.add(new CountryItem(lines[i]));
         } catch (IOException e) {
             // Should never happen!
